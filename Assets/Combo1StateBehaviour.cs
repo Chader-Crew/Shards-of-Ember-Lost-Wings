bf1a464e6a1f0c0028a8ef10c6c05cbb5329d163
Alu <vitinh.do.cs@gmail.com>
2024-05-02 12:48:56 -0300

    State Machine de Ataque
    Tenho que consultar com vocês ainda por causa da ideia la de chain of responsibility mas eu acho que fazer assim pelo animator dá bom

---
+++ Assets/Combo1StateBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//comportamento de parte de combo linear. Desliga o trigger de proximo ataque se nao tiver apertado durante a janela de buffer.
+public class Combo1StateBehaviour : StateMachineBehaviour
+{
+    [SerializeField] private float bufferTime;
+    private float normalizedBuffer;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        normalizedBuffer = 1- bufferTime / stateInfo.length;
+        Debug.Log($"Normalized animation buffer for {animator.name}: {normalizedBuffer}");
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if(animator.GetBool("ataka") == true)
+        {
+            if(stateInfo.normalizedTime < bufferTime)
+            {
+                animator.ResetTrigger("ataka");
+            }
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
