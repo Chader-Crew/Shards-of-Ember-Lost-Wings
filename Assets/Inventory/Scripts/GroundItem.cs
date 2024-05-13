bc1241c07e398bd84f5f831d94193e655621e7df
anaclaragl <ag.linhares2@gmail.com>
2024-05-12 23:07:57 -0300

    Inventario, Toolbar, Drag and Drop

---
+++ Assets/Inventory/Scripts/GroundItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundItem : MonoBehaviour
+{
+    public Item item;
+
+    public void Initialize(Item item){
+        this.item = item;
+    }
+}
