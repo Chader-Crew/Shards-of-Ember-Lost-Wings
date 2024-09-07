88ac4dab69aa09f67a8255f1ad8b41dac8508b87
TiagoANeto <tiago98.ta@gmail.com>
2024-09-07 17:56:37 -0300

    logica inicial da skill

---
+++ Assets/Scripts/Combat/SkillAppliers/CreateThorns.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateThorns : MonoBehaviour
+{
+    [SerializeField] private CooldownDisplay cooldownDisplay;
+    [SerializeField] private SkillBase skill;
+    [SerializeField] private SkillData skillData;
+
+    private void Thorns()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            skillData.Target(other.gameObject.GetComponent<Character>());
+            skill.Activate(skillData);
+        }
+        Destroy(gameObject);
+    }
+}
