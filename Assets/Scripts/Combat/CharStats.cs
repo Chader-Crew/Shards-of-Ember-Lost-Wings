8c8a27edd652cc27bfbc225c89f5e18179bec565
Alu <vitinh.do.cs@gmail.com>
2024-09-13 12:31:32 -0300

    Funcionalidade "stats temporários" p/ charStats
    - adicionada uma classe de extensões para MonoBehaviours, e uma função estática para chamadas de método depois de delay usando corotina (pra poder passar argumentos sem ficar criando uma corotina pra cada coisa)
    
    - adicionada funcionalidade de métodos "stats temporários" ao charStats, que aplicam uma alteração nos stats que é removida depois de tantos segundos.

--- Assets/Scripts/Combat/CharStats.cs
+++ Assets/Scripts/Combat/CharStats.cs
@@ -9,4 +9,36 @@ public class CharStats : MonoBehaviour
     public float hp;
     public int atk, def, spd, maxHp;
 
+    #region Temporary Stats Methods
+    public void TemporaryAtk(int variation, float time)
+    {
+        atk += variation;
+        this.CallWithDelay(() => atk -= variation, time);
+    }
+
+    public void TemporaryDef(int variation, float time)
+    {
+        def += variation;
+        this.CallWithDelay(() => def -= variation, time);
+    }
+
+    public void TemporarySpd(int variation, float time)
+    {
+        spd += variation;
+        this.CallWithDelay(() => spd -= variation, time);
+    }
+
+    public void TemporaryMaxHp(int variation, float time)
+    {
+        maxHp += variation;
+        this.CallWithDelay(() => maxHp -= variation, time);
+    }
+
+    public void TemporaryHp(int variation, float time)
+    {
+        hp += variation;
+        this.CallWithDelay(() => hp -= variation, time);
+    }
+    #endregion
+
 }
