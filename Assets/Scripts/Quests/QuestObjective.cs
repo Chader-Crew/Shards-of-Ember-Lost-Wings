e42d7df6f7721f945ab0a9fa198a024ab397a600
anaclaragl <ag.linhares2@gmail.com>
2024-12-06 11:45:16 -0300

    nao sei o que eu fiz aqui

--- Assets/Scripts/Quests/QuestObjective.cs
+++ Assets/Scripts/Quests/QuestObjective.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public abstract class QuestObjective : QuestData
 {
+    //public QuestParent[] parents; //quantos objetos compartilham da mesma quest
+
     public abstract void Objective();
 }
