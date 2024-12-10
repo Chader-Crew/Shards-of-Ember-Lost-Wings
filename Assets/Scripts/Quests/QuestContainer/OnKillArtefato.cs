4e20fce23da565c105d36abc83f8aa26efc58370
Alu <vitinh.do.cs@gmail.com>
2024-12-09 23:16:16 -0300

    AAAAA

--- Assets/Scripts/Quests/QuestContainer/OnKillArtefato.cs
+++ Assets/Scripts/Quests/QuestContainer/OnKillArtefato.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,16 @@ public class OnKillArtefato : MonoBehaviour
     public DialogueManager dialogueManager, dialogueManager2; //manager da nyxtra de dialogo
     //[SerializeField] private QuestData questData;
 
-    public void OnDestroy()
+    private void Start()
     {
-        //if (questData != questManager.GetCurrentQuest()) return;
-        QuestManager.Instance._altarIsActive = true;
-        questManager.CompleteCurrentQuest();
+        Character character = GetComponent<Character>();
+        character.OnDiedEvent += () =>
+        {
+            QuestManager.Instance._altarIsActive = true;
+            questManager.CompleteCurrentQuest();
 
-        dialogueManager.enabled = false; 
-        dialogueManager2.enabled = true; 
-
+            dialogueManager.enabled = false;
+            dialogueManager2.enabled = true;
+        };
     }
 }
